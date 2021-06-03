100c80f328c69fdf4ef3460f22d194e84e451234
Cristofer Martinez <cristoferm@gmail.com>
2021-06-03 15:56:07 -0700

    issue-5-user-can-steer-their-car | Cristofer Martinez

--- main.js
+++ main.js
@@ -3,8 +3,11 @@ var gameKeys = ['ArrowLeft', 'ArrowUp', 'ArrowRight', 'ArrowDown', ' '];
 var $car = document.querySelector('.car');
 
 var carStyle = $car.style;
-var leftRight = 1;
 var currentX = 1;
+var currentY = 1;
+var leftRight = 10;
+var upDown = 0;
+var way = 'x';
 
 var ignition = false;
 var gas = null;
@@ -26,10 +29,35 @@ function keyHandler(event) {
     }
   } else {
     $car.className = 'car ' + event.key;
+    if (event.key === 'ArrowRight') {
+      leftRight = 10;
+      upDown = 0;
+      way = 'x';
+    }
+    if (event.key === 'ArrowLeft') {
+      leftRight = -10;
+      upDown = 0;
+      way = 'x';
+    }
+    if (event.key === 'ArrowDown') {
+      leftRight = 0;
+      upDown = 10;
+      way = 'y';
+    }
+    if (event.key === 'ArrowUp') {
+      leftRight = 0;
+      upDown = -10;
+      way = 'y';
+    }
   }
 }
 
 function direction() {
-  currentX = parseInt(carStyle.left);
-  carStyle.left = (currentX + leftRight) + 'px';
+  if (way === 'x') {
+    currentX = parseInt(carStyle.left);
+    carStyle.left = (currentX + leftRight) + 'px';
+  } else {
+    currentY = parseInt(carStyle.top);
+    carStyle.top = (currentY + upDown) + 'px';
+  }
 }
